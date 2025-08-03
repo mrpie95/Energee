8f28b46ffd4b15f3970e0be4a35fad2db9e3f9ad
mrpie95 <michael.pieniazek@gmail.com>
2025-08-03 11:02:25 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20, fontFamily: 'sans-serif', color: '#fff' }}>
+      <h1>Energy Tracker</h1>
+      <p>This is a working fullscreen PWA demo.</p>
+    </div>
+  )
+}
